61d0104fdc446c05fb70cd5d96dca43082df0c6c
David Onuh <davidonuh1@gmail.com>
2025-04-09 09:28:51 +0100

    Restructure format

--- db_perf/perf.py
+++ db_perf/perf.py
@@ -1,4 +1,5 @@
 import time
+from typing import Dict
 
 import pandas as pd
 
@@ -6,9 +7,9 @@ from db_perf.db_versions.base import BaseClient
 
 
 class PerfClient:
+
     def __init__(self, clients: list[BaseClient]):
         self.clients = clients
-        self.results = {}
 
     def run_insert_benchmark(self, num_records: int):
         for client in self.clients:
@@ -18,18 +19,21 @@ class PerfClient:
             payload = client.generate_insert_payload(
                 num_records
             )  # generates List[Event]
-            start = time.perf_counter()
+            #start = time.perf_counter()
             client.batch_inserts(payload)
-            elapsed = time.perf_counter() - start
+            #elapsed = time.perf_counter() - start
 
-            self.results.setdefault(label, {})["insert_time_sec"] = elapsed
+            #self.results.setdefault(label, {})["insert_time_sec"] = elapsed
 
-    def run_query_benchmark(self):
+    def run_query_benchmark(self) -> Dict[str, Dict[str, float]]:
+        results = {}
         for client in self.clients:
             label = client._get_correct_schema_path().name
             print(f"Running query benchmark for Client: {label}")
             result = client.benchmark_queries()
-            self.results[label] = result
+            results[label] = result
+        return results
 
-    def to_dataframe(self):
-        return pd.DataFrame.from_dict(self.results, orient="index")
+    def to_dataframe(self, results: dict):
+        return pd.DataFrame.from_dict(results, orient="index")
+
